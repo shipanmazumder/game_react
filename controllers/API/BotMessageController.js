3d726d4ea4732c009ef3f10475b6f0918e91a5c1
Shipan Mazumder <shipanmazumder@gmail.com>
2021-01-17 22:04:01 +0600

    webhook

--- controllers/API/BotMessageController.js
+++ controllers/API/BotMessageController.js
@@ -1,3 +1,16 @@
+const path = require("path");
+const fs = require("fs");
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    "log",
+    "log.json"
+);
 exports.webHookGet = (req, res, next) => {
-   console.log(req.body)
-  };
+    console.log(req.body);
+    fs.writeFile('log.json', JSON.stringify(req.body), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+      res.send("s");
+};
